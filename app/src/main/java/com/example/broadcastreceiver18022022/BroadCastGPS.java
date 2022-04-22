cb8cab1991cd4aedbccf5684b9f59e4d133df017
Phat <phatdroid94@gmail.com>
2022-04-22 18:14:31 +0700

    provide change

--- app/src/main/java/com/example/broadcastreceiver18022022/BroadCastGPS.java
+++ app/src/main/java/com/example/broadcastreceiver18022022/BroadCastGPS.java
@@ -3,11 +3,17 @@ package com.example.broadcastreceiver18022022;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 public class BroadCastGPS extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        if (intent.getAction().matches("android.location.PROVIDERS_CHANGED")) {
+            Toast.makeText(context, "in android.location.PROVIDERS_CHANGED",
+                    Toast.LENGTH_SHORT).show();
+            Log.d("BBB","Provide changed");
+        }
     }
 }
