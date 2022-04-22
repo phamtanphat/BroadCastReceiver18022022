a01668364bcd00400b09cc0b55302aec28e487f5
Phat <phatdroid94@gmail.com>
2022-04-22 18:08:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiver18022022/BroadCastGPS.java
@@ -0,0 +1,13 @@
+package com.example.broadcastreceiver18022022;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadCastGPS extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
