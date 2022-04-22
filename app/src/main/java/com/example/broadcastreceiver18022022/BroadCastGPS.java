e9bb18af11b92fd8b1469cc7e0b79bd7ae1afa04
Phat <phatdroid94@gmail.com>
2022-04-22 18:48:00 +0700

    setup location

--- app/src/main/java/com/example/broadcastreceiver18022022/BroadCastGPS.java
+++ app/src/main/java/com/example/broadcastreceiver18022022/BroadCastGPS.java
@@ -1,8 +1,12 @@
 package com.example.broadcastreceiver18022022;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -10,10 +14,22 @@ public class BroadCastGPS extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().matches("android.location.PROVIDERS_CHANGED")) {
-            Toast.makeText(context, "in android.location.PROVIDERS_CHANGED",
-                    Toast.LENGTH_SHORT).show();
-            Log.d("BBB","Provide changed");
+        if (intent.getAction().matches(LocationManager.PROVIDERS_CHANGED_ACTION)) {
+            String locationMode = "";
+            ContentResolver contentResolver = context.getContentResolver();
+            // Find out what the settings say about which providers are enabled
+            int mode = Settings.Secure.getInt(
+                    contentResolver, Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF);
+
+            if (mode != Settings.Secure.LOCATION_MODE_OFF) {
+                Log.d("BBB",locationMode);
+                Intent intentLocation = new Intent(context,MainActivity.class);
+                intentLocation.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intentLocation.putExtra("mode", true);
+                context.startActivity(intentLocation);
+            }else {
+                Log.d("BBB","Location off");
+            }
         }
     }
 }
--- app/src/main/java/com/example/broadcastreceiver18022022/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver18022022/MainActivity.java
@@ -1,14 +1,114 @@
 package com.example.broadcastreceiver18022022;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private LocationListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        configure_location();
+
+        listener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("BBB","Lat : " +location.getLatitude());
+                Log.d("BBB","Long : " +location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+//                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivity(i);
+            }
+        };
+
+        Intent intent = getIntent();
+
+        if (intent != null){
+            boolean mode = intent.getBooleanExtra("mode",false);
+            if (mode){
+                configure_location();
+            }
+        }
+    }
+
+    private void configure_location(){
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                request_permission();
+            }
+        } else {
+            // permission has been granted
+            locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+        }
+    }
+
+    private void request_permission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                ACCESS_COARSE_LOCATION)) {
+
+            Snackbar.make(findViewById(R.id.main_linear_layout), "Location permission is needed because ...",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("retry", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+                        }
+                    })
+                    .show();
+        } else {
+            // permission has not been granted yet. Request it directly.
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_location();
+                break;
+            default:
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
